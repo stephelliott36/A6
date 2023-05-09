13cea023843853b6ee7752a431ca164ba86798b4
Stephen Elliott <sge2@princeton.edu>
2023-05-09 18:13:01 -0400

    done

--- createdataA.c
+++ createdataA.c
@@ -21,18 +21,22 @@ int main() {
     /* writes my name to the file */
     fprintf(psFile, "%s", name);
     /* 0x420068 mov w0, 'A' */ 
-
+    /* null byte for padding */
     putc('\0', psFile);
     
+    /* moves A into register w0*/
     temp = MiniAssembler_mov(w0, a);
     fwrite(&temp, sizeof(unsigned int), 1, psFile);
 
+    /* address of grade into register */
     temp = MiniAssembler_adr(x1, grade, 0x42006c);
     fwrite(&temp, sizeof(unsigned int), 1, psFile);
 
+    /*stores the byte in w0 in x1 */
     temp =  MiniAssembler_strb(w0, x1);
     fwrite(&temp, sizeof(unsigned int), 1, psFile);
 
+    /* branches to is your grade command */
     temp = MiniAssembler_b(ulAddr, ulAddrOfThisInstr);
     fwrite(&temp, sizeof(unsigned int), 1, psFile);
     
@@ -41,7 +45,7 @@ int main() {
        /* fprintf(psFile,  "%c", '\0'); */
        putc('\0', psFile);
     }
-    /* address of grade = B */
+    /* address of end of my name and beginning of instructions */
     
     fwrite(&ul, sizeof(unsigned long), 1, psFile); 
 
--- miniassembler.c
+++ miniassembler.c
@@ -20,23 +20,38 @@ static void setField(unsigned int uiSrc, unsigned int uiSrcStartBit,
                      unsigned int uiNumBits)
 {
    /* Your code here */
-   unsigned int mask;
+   unsigned int mask; /* all 1s, length of uiNumBits */
+   /* starts uiSrc at uiSrcStartBit*/
    uiSrc = uiSrc >> uiSrcStartBit;
    mask = (1<<uiNumBits) - 1;
+
+   /* writes to uiSrc */
    uiSrc = mask & uiSrc;
-   uiSrc = uiSrc <<uiDestStartBit;
+   uiSrc = uiSrc <<uiDestStartBit; 
+   /* writes to puiDest from uiSrc 
+   which has been offset by uiDestStartBit*/
    *puiDest = *puiDest | uiSrc;
 
 
 }
 
-/*--------------------------------------------------------------------*/
+/* Return the machine language equivalent of "adr reg, addr".
+
+   Parameters:
+      uiReg: the number of reg. 0 <= uiReg <= 31.
+      ulAddr: the address denoted by addr.
+      ulAddrOfThisInstr: the address of the adr instruction itself
+                         (must be a multiple of 4).     */
 
 unsigned int MiniAssembler_mov(unsigned int uiReg, int iImmed)
 {
    /* Your code here */
+
    unsigned int uiInstr;
+   /* Base instruction code */
    uiInstr = 0x52800000;
+
+   /* inserts uiReg into first 5 bits and iImmed into next 16 */
    setField(uiReg, 0, &uiInstr, 0, 5);
    setField(iImmed, 0, &uiInstr, 5, 16);
    
@@ -73,22 +88,32 @@ unsigned int MiniAssembler_adr(unsigned int uiReg, unsigned long ulAddr,
    return uiInstr;
 }
 
-/*--------------------------------------------------------------------*/
-/******/
+/* Return the machine language equivalent of "strb fromreg,[toreg]",
+   where fromreg is a W register and toreg is a X register.
+
+   Parameters:
+      uiFromReg: the number of fromreg. 0 <= uiFromReg <= 31.
+      uiToReg: the number of toreg.     0 <= uiToReg <= 31.      */
 unsigned int MiniAssembler_strb(unsigned int uiFromReg,
    unsigned int uiToReg)
 {
    /* Your code here */
    unsigned int uiInstr;
-   /* unsigned int uiDisp; */
+   /* Base instruction code */
    uiInstr = 0x39000000;
-   /* uiDisp = ; ***** ASK ****/
+   /* places uiFromReg in first 5 bits and uiToReg in next 5*/
    setField(uiFromReg, 0, &uiInstr, 0, 5); 
    setField(uiToReg, 0, &uiInstr, 5, 5); 
    return uiInstr;
 }
 
-/*--------------------------------------------------------------------*/
+/* Return the machine language equivalent of "b addr".
+
+   Parameters:
+      ulAddr: the address denoted by addr, that is, the address to
+         which the branch should occur (must be a multiple of 4).
+      ulAddrOfThisInstr: the address of the b instruction itself
+         (must be a multiple of 4).  */
 
 unsigned int MiniAssembler_b(unsigned long ulAddr,
    unsigned long ulAddrOfThisInstr)
@@ -96,8 +121,11 @@ unsigned int MiniAssembler_b(unsigned long ulAddr,
    /* Your code here */
     unsigned int uiInstr;
    unsigned int uiDisp = ulAddr - ulAddrOfThisInstr;
+   /* Base instruction code */
    uiInstr = 0x14000000;
-  
+   
+   /* sets uiDisp, shifted by 2 because it will
+    be multiplied by 4 when read */
    setField(uiDisp, 2, &uiInstr, 0, 26); 
    return uiInstr;
 
